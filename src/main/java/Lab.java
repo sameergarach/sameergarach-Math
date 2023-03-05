c9fd572341dc5bd608d811032153db1d2cc5a4aa
Sameer Garach <sameer.garach@gmail.com>
2023-03-05 20:33:19 +0000

    initial commit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -21,6 +21,6 @@ public class Lab {
      * @return return a^b using the pow() method of the Math class.
      */
     public double mathPow(double a, double b){
-        return pow(a,b);
+        return Math.pow(a,b);
     }
 }
